14f41421103babecc7d298f4833f049d8ebc9201
Theo Peudupin <theo.peudupin@gmail.com>
2025-01-27 12:17:40 +0100

    Add fonction spinning and drop down stats

--- src/main.js
+++ src/main.js
@@ -1,31 +1,36 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// =======================
+// Three.JS initialisation
+// =======================
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 3;
 const canvas = document.getElementById("canvathree");
-
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 renderer.setClearColor( 0xffffff, 0);
 document.getElementById('three').appendChild( renderer.domElement );
 
-// Windows Resizing
-window.addEventListener( 'resize', onWindowResize, false );
+// =======================
+// Variables and constants
+// =======================
+var mouseDown = false, mouseX = 0, mouseY = 0;
+var cookieRotationSpeedX = 0.005, cookieRotationSpeedY = 0.005;
+var cookie;
+var statsHidden=true;
 
+// =========
+// Fonctions
+// =========
 function onWindowResize(){
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
-
-// Rotate with mouse
-var mouseDown = false,
-    mouseX = 0,
-    mouseY = 0;
 
 function onMouseMove(evt) {
     if (!mouseDown) {
@@ -81,31 +86,62 @@ function zoomScene(deltaY) {
 	scene.position.z -= deltaY / 200;
 	document.getElementById('wheelDeltaY').innerHTML = deltaY.toFixed(6);
 }
-addMouseHandler(canvas);
 
-// Add Cookie
-var cookie;
+function cookieRotationSpeed(){
+    cookieRotationSpeedY += 0.5;
+}
+
 function addCookie() {
 	const loader = new GLTFLoader();
 	loader.load( 'cookie/scene.gltf', function ( gltf ) {
-		cookie = gltf.scene;
-		scene.add(cookie);
+        cookie = gltf.scene;
+        scene.add(cookie);
 	}, undefined, function ( error ) {
 		console.error( error );
 	} );
 }
+
+function toggleStats(){
+    if(statsHidden){
+        // Show Stats
+        document.getElementById('dropdownstatsbuttonup').style.display = 'block';
+        document.getElementById('dropdownstatsbuttondown').style.display = 'none';
+        document.getElementById('dropdownstats').style.display = 'block';
+        statsHidden=false;
+    }else{
+        // Hide Stats
+        document.getElementById('dropdownstatsbuttonup').style.display = 'none';
+        document.getElementById('dropdownstatsbuttondown').style.display = 'block';
+        document.getElementById('dropdownstats').style.display = 'none';
+        statsHidden=true;
+    }
+}
+
+// ==============
+// Event listener
+// ==============
+document.getElementById('rotationCookie').addEventListener('click', cookieRotationSpeed, false);
+document.getElementById('dropdownstatsbuttonup').addEventListener('click', toggleStats, false);
+document.getElementById('dropdownstatsbuttondown').addEventListener('click', toggleStats, false);
+addMouseHandler(canvas);
+window.addEventListener('resize', onWindowResize, false);
+
+// ====
+// Main
+// ====
 addCookie();
 
 function animate() {
 
-	if(cookie && !mouseDown) {
-
-		cookie.rotation.x += 0.005;
-		cookie.rotation.y += 0.01;
-
-		document.getElementById('cookieRotationX').innerHTML = cookie.rotation.x.toFixed(6);
-		document.getElementById('cookieRotationY').innerHTML = cookie.rotation.y.toFixed(6);
-	}
+	if(cookie) 
+    {
+        if(cookieRotationSpeedY > 0.005) cookieRotationSpeedY -= 0.005;
+        cookie.rotation.x += cookieRotationSpeedX;
+        cookie.rotation.y += cookieRotationSpeedY;
+        document.getElementById('cookieRotationX').innerHTML = cookie.rotation.x.toFixed(6);
+        document.getElementById('cookieRotationY').innerHTML = cookie.rotation.y.toFixed(6);
+    }
+    
 
 	document.getElementById('sceneRotationX').innerHTML = scene.rotation.x.toFixed(6);
 	document.getElementById('sceneRotationY').innerHTML = scene.rotation.y.toFixed(6);
